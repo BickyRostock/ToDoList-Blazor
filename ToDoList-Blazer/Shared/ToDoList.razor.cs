93c3676d9af7b18a4b1d23ed7223e1933b287508
Ricky Bostock <RickyB@BriefYourMarket.co.uk>
2021-10-09 13:29:01 +0100

    Allow management of to-do list. Removed notion of a view model and use a component base inheritance idea instead for state management. This is more in line with the blazor framework 'way fo doing things' I guess.

--- ToDoList-Blazer/Shared/ToDoList.razor.cs
+++ ToDoList-Blazer/Shared/ToDoList.razor.cs
@@ -1,32 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using ToDoList_Blazer.Data;
+using ToDoList_Blazer.Shared.ViewModel;
 
-namespace ToDoList_Blazer.Shared.ViewModel
+namespace ToDoList_Blazer.Shared
 {
-    public class ToDoListViewModel
+    public class ToDoListStateHandler : ComponentBase
     {
         public ToDoItem[] ToDoList { get; set; }
 
         [Required]
         [MaxLength(100, ErrorMessage = "The 'What' is too long, maybe add the detail to notes")]
         public string What { get; set; }
+
         [Required]
         public DateTime When { get; set; }
+
         [MaxLength(50, ErrorMessage = "Woah there, is this for a whole army!?")]
         public string Who { get; set; }
+
         [MaxLength(300, ErrorMessage = "Steady on there, this doesn't have to be war and peace!")]
         public string Notes { get; set; }
 
-        private IToDoItemService m_toDoItemService;
+        [Inject]
+        private IToDoItemService m_toDoItemService { get; set; }
 
-        public ToDoListViewModel(IToDoItemService service)
+
+        protected override Task OnInitializedAsync()
         {
-            m_toDoItemService = service;
             ToDoList = m_toDoItemService.GetAll();
+
+            return base.OnInitializedAsync();
         }
 
         public async void OnNewToDoItemSubmitted()
@@ -43,6 +50,17 @@ namespace ToDoList_Blazer.Shared.ViewModel
             await m_toDoItemService.CreateAsync(newItem);
 
             ToDoList = ToDoList.Append(newItem).ToArray();
+
+            StateHasChanged();
+        }
+
+        public async void DeleteItemHandler(ToDoItem itemToDelete)
+        {
+            await m_toDoItemService.DeleteAsync(itemToDelete);
+
+            ToDoList = ToDoList.Where(item => item.Id != itemToDelete.Id).ToArray();
+
+            StateHasChanged();
         }
     }
 }
